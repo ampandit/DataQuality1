02b44bd401f672edf403d362273d64c6a7e05129
Amy Pandit <APandit6@humana.com>
2018-06-18 13:11:44 -0400

    added reviews page, made minor edits for tablelist

---
+++ DataQuality1/CommonConstants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humana.IMA.DataMarketplace.Common.Core.Constants
+{
+    public sealed class CommonConstants
+    {
+        public sealed class Encryption
+        {
+            public const string Salt = "837";
+            public const string Key = "-=!@#$%^&*()=-";
+        }
+
+        public sealed class PodiumRequestPaths
+        {
+            public const string GetEntitiesByCrit = "entity/v1/getEntitiesByCrit/6/";
+            public const string Login = "j_spring_security_check";
+            public const string Logout = "j_spring_security_logout";
+        }
+
+        public sealed class SessionCookieKey
+        {
+            public const string CookieBase = "PodiumCookienBase";
+            public const string SessionId = "PodiumSession";
+        }
+
+        public const int PageSizeForResults = 6;
+    }
+}
